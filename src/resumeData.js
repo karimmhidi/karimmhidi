40389c49e2c9b027daf0f83945c899a9cd349c00
zaghmi siwar <zaghmi.siwar@gmail.com>
2021-12-30 18:58:28 +0100

    porftolio

---
+++ src/resumeData.js
@@ -0,0 +1,70 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Karim Mhidi",
+    "role": "Frontend Developer and Data Scientist",
+    "Email":"karimmhidi1@gmail.com",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies and simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/KarimMhidi/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/karimmhidi",
+          "className":"fa fa-github"
+        },
+        
+      ],
+    "aboutme":"I am currently a pre-final year student at Ihec Carthage . I am a  Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Tunisia",
+    "website":"https://karimMhidi.github.io",
+    "skillsDescription":"My skills ",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/pixel.png"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/pixel.png"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/pixel.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/pixel.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Think Big Go Big",
+        
+      },
+      {
+        "description":"Never Back Down",
+        
+      }
+    ]
+  }
+  
+  export default resumeData
