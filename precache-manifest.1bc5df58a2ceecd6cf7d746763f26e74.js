6c26b9e3ab855e61953e60ecf69d9d9c38e437d5
zaghmi siwar <zaghmi.siwar@gmail.com>
2021-12-30 19:10:55 +0100

    Updates

---
+++ precache-manifest.1bc5df58a2ceecd6cf7d746763f26e74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd356ecab58c8d725061",
+    "url": "/karimmhidi/static/css/main.958ff5b2.chunk.css"
+  },
+  {
+    "revision": "dd356ecab58c8d725061",
+    "url": "/karimmhidi/static/js/main.dd356eca.chunk.js"
+  },
+  {
+    "revision": "1048638a4991ffad96d3",
+    "url": "/karimmhidi/static/js/1.1048638a.chunk.js"
+  },
+  {
+    "revision": "615f0a3e00e6a619ffe7",
+    "url": "/karimmhidi/static/js/runtime~main.615f0a3e.js"
+  },
+  {
+    "revision": "01b35b511a49da7f7393c2031bea807b",
+    "url": "/karimmhidi/index.html"
+  }
+];
